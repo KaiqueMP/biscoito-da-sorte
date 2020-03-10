332b3e949d5ce05c9d2d60f6f6283b96aad98463
Kaique <artes.kaiquemarques@hotmail.com>
2020-03-10 08:58:35 -0300

    Corrigido
    1 - Concertado o erro do gerador de numeros aleatórios.
    
    2 - Botão estilizado

--- src/App.js
+++ src/App.js
@@ -12,19 +12,23 @@ class App extends Component{
     this.abreBiscoito = this.abreBiscoito.bind(this);
 
     this.frases = [
-      'Frase 1',
-      'Frase 2',
-      'Frase 3',
-      'Frase 4',
-      'Frase 5']
+      '"A vida trará coisas boas se tiveres paciência."',
+      '"Defeitos e virtudes são apenas dois lados da mesma moeda."',
+      '"A maior de todas as torres começa no solo."',
+      '"Demonstre amor e alegria em todas as oportunidades e verás que a paz nasce dentro de você."',
+      '"Não compense na ira o que lhe falta na razão."']
+      
   }
 
   abreBiscoito(){
     let state = this.state;
 
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.lenght);
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
     state.textoFrase = this.frases[numeroAleatorio];
     this.setState(state);
+    console.log(this.frases);
+    console.log(this.textoFrase);
+    console.log(this.numeroAleatorio);
 
 
   }
@@ -33,7 +37,7 @@ class App extends Component{
     return (
       <div className="container">
           <img src={require('./assets/biscoito.png')} className="img"/>
-          <Botao nome="Abrir biscoito" acaoBtn={this.abreBiscoito}/>
+          <Botao nome="Abrir biscoito" acaoBtn={this.abreBiscoito} />
           <h3 className="textoFrase">{this.state.textoFrase}</h3>
       </div>
     );
@@ -44,7 +48,7 @@ class Botao extends Component{
   render(){
     return(
       <div>
-        <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
+        <button className="cookieButton" onClick={this.props.acaoBtn}>{this.props.nome}</button>
       </div>
     );
   }
